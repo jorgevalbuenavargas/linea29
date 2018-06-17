803eaf61499c8cea0802b5cee3f1d6aa16e88c6c
jorgevalbuenavargas <jorge.avalbuena@gmail.com>
2018-06-17 19:53:02 -0300

    new stops

--- sql_inserts/INSERT_BRANCHES.sql
+++ sql_inserts/INSERT_BRANCHES.sql
@@ -1,4 +1,6 @@
-INSERT INTO branches(id,name,schedule,created_at,updated_at) VALUES (1,'A - Olivos','24 horas','2018-04-07 16:28:06','2018-04-07 17:35:33');
-INSERT INTO branches(id,name,schedule,created_at,updated_at) VALUES (2,'B - Parque Sarmiento','06:00 a 20:00','2018-04-07 16:28:25','2018-04-07 16:28:25');
-INSERT INTO branches(id,name,schedule,created_at,updated_at) VALUES (3,'C - Estacion Rivadavia','24 horas','2018-04-07 16:28:41','2018-05-04 18:59:00');
+INSERT INTO branches(id,name,schedule,created_at,updated_at) VALUES (1,'A - Av. Scalabrini Ortiz','24 horas','2018-04-07 16:28:06','2018-06-17 22:39:03');
+INSERT INTO branches(id,name,schedule,created_at,updated_at) VALUES (2,'B - Plaza Italia','06:00 a 20:00','2018-04-07 16:28:25','2018-06-17 22:39:12');
+INSERT INTO branches(id,name,schedule,created_at,updated_at) VALUES (3,'C - Av. Luis María Campos','24 horas','2018-04-07 16:28:41','2018-06-17 22:39:27');
 INSERT INTO branches(id,name,schedule,created_at,updated_at) VALUES (4,'D - Estacion Belgrano C','06:00 a 20:00','2018-04-07 16:28:59','2018-05-04 18:59:07');
+INSERT INTO branches(id,name,schedule,created_at,updated_at) VALUES (5,'E - Avenida Forest','24 horas','2018-06-17 22:34:07','2018-06-17 22:34:07');
+INSERT INTO branches(id,name,schedule,created_at,updated_at) VALUES (6,'F - Av. Dorrego','06:00 a 20:00','2018-06-17 22:39:46','2018-06-17 22:39:46');
